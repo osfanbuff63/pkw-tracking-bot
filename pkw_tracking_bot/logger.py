3561c36533c9033f343ed8dc81e0416c41062884
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2023-12-27 13:49:14 -0500

    me when v2

--- pkw_tracking_bot/logger.py
+++ pkw_tracking_bot/logger.py
@@ -3,14 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import logging
+import sys
 
 from .pathlib_ext import PathExt as Path
 
 file = Path("pkw_tracking_bot.log")
 file.write("")
-# handler = logging.FileHandler(
-#    filename="pkw_tracking_bot.log", encoding="utf-8", mode="w"
-# )
+handler = logging.FileHandler(
+    filename="pkw_tracking_bot.log", encoding="utf-8", mode="w"
+)
+handler2 = logging.StreamHandler(sys.stdout)
 logger = logging.getLogger("pkw_tracking_bot")
-# logger.addHandler(handler)
+logger.addHandler(handler)
+logger.addHandler(handler2)
 logger.setLevel(logging.DEBUG)
