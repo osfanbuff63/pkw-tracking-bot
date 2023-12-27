607a0656bf172fa5e1ab0d2d0f4d4a72345a838a
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2023-12-27 13:48:51 -0500

    Log to stdout in addition to a file

--- pkw_tracking_bot/logger.py
+++ pkw_tracking_bot/logger.py
@@ -8,9 +8,9 @@ from .pathlib_ext import PathExt as Path
 
 file = Path("pkw_tracking_bot.log")
 file.write("")
-handler = logging.FileHandler(
-    filename="pkw_tracking_bot.log", encoding="utf-8", mode="w"
-)
+# handler = logging.FileHandler(
+#    filename="pkw_tracking_bot.log", encoding="utf-8", mode="w"
+# )
 logger = logging.getLogger("pkw_tracking_bot")
-logger.addHandler(handler)
+# logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
