169c7db0926d28a41d3597da6f80e49394f3ae09
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2024-02-26 10:26:55 -0500

    fix(archive stats): :bug: put database.toml on the end :facepalm:

--- pkw_tracking_bot/embeds.py
+++ pkw_tracking_bot/embeds.py
@@ -172,7 +172,7 @@ def stats_embed(user: User | Member, year: int, month: int) -> Embed:
         "08": "August",
         "09": "September",
     }
-    database = Database(PathExt(f"./.archive/{year}/{month}"))
+    database = Database(PathExt(f"./.archive/{year}/{month}/database.toml"))
     stats = database.get("id")
     logger.debug(f"stats: {stats}")
     text = f"**Course 1**: {stats["course_1"] if stats["course_1"] != "99:99.99" else "*No time submitted*"}\n"
